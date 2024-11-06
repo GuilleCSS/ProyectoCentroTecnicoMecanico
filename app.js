2f01f71fad0672ace3075912d989c4f874e97aa9
sergiodjcm <sergiodejesuscastillomolano@gmail.com>
2024-11-06 16:57:10 -0600

    CORS

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cors = require('cors'); // Importa el módulo de CORS
 require('./config/Database'); // Importa la conexión a la base de datos
+
 const vehiculosRoutes = require('./routes/vehiculos');
 const citasRoutes = require('./routes/citas');
 const clientesRoutes = require('./routes/clientes');
 
 const app = express();
+
+// Configuración de CORS
+const corsOptions = {
+  origin: ['http://localhost:3000/clientes,http://localhost:3000/vehiculos, http://localhost:3000/citas'], // Cambia a los dominios permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Habilita el envío de cookies y encabezados de autenticación
+};
+
+// Usa CORS con las opciones configuradas
+app.use(cors(corsOptions));
+
+// Middlewares
 app.use(express.json());
 
 // Rutas
